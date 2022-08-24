1aa2bf38f741e82da4ce2c117d6ea6407262d161
solomon galleh <solomongalleh@gmail.com>
2022-08-24 18:06:16 +0100

    react stripe payment

---
+++ reactstripe/src/Success.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Success = () => {
+  return (
+    <div className="success">
+      <img src="./image.pnng" alt="Logo" />
+      <h2>Your Payment Has Been Made Successfully</h2>
+    </div>
+  );
+};
+
+export default Success;
